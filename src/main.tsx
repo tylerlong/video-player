b75c4f5e7306438c1296f0303cd9f29b0a22b113
Tyler Long <tyler4long@gmail.com>
2020-07-04 20:02:09 -0700

    Refactor UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,54 @@
 import React from 'react';
 import {Component} from 'react-subx';
-import {Spin} from 'antd';
-import store from './store';
+import {Spin, Button} from 'antd';
+import {StoreType} from './store';
 
-export interface Props {
-  store: typeof store;
-}
+type PropsStore = {
+  store: StoreType;
+};
 
-class App extends Component<Props> {
+type PropsDevices = {
+  devices: MediaDeviceInfo[];
+  kind: string;
+};
+
+class App extends Component<PropsStore> {
   render() {
     const store = this.props.store;
     return store.ready ? <Main store={store} /> : <Spin size="large" />;
   }
 }
 
-class Main extends Component<Props> {
+class Main extends Component<PropsStore> {
   render() {
     const store = this.props.store;
     return (
       <>
-        Video Input:{' '}
-        <select id="video-input">
-          {store.devices
-            .filter(d => d.kind === 'videoinput')
-            .map(d => (
-              <option key={d.deviceId} value={d.deviceId}>
-                {d.label}
-              </option>
-            ))}
-        </select>
-        Audio Input:{' '}
-        <select id="audio-input">
-          {store.devices
-            .filter(d => d.kind === 'audioinput')
-            .map(d => (
-              <option key={d.deviceId} value={d.deviceId}>
-                {d.label}
-              </option>
-            ))}
-        </select>
-        Audio Output:{' '}
-        <select id="audio-output">
-          {store.devices
-            .filter(d => d.kind === 'audiooutput')
-            .map(d => (
-              <option key={d.deviceId} value={d.deviceId}>
-                {d.label}
-              </option>
-            ))}
-        </select>
+        Video Input: <Devices devices={store.devices} kind="videoinput" />
+        Audio Input: <Devices devices={store.devices} kind="audioinput" />
+        Video Output: <Devices devices={store.devices} kind="audiooutput" />
+        <Button onClick={() => store.play()}>Play</Button>
         <hr />
         <video width="800" height="600" id="video-player"></video>
       </>
+    );
+  }
+}
+
+class Devices extends Component<PropsDevices> {
+  render() {
+    const {devices, kind} = this.props;
+    return (
+      <select id={kind} onChange={e => console.log(e.target.value)}>
+        {devices
+          // .filter(d => d.kind === kind && d.deviceId !== 'default')
+          .filter(d => d.kind === kind)
+          .map(d => (
+            <option key={d.deviceId} value={d.deviceId}>
+              {d.label}
+            </option>
+          ))}
+      </select>
     );
   }
 }
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,41 @@
 import SubX from 'subx';
 
-const store = SubX.proxy({
+export type StoreType = {
+  ready: boolean;
+  devices: MediaDeviceInfo[];
+  init: () => void;
+  videoInput?: MediaDeviceInfo;
+  audioInput?: MediaDeviceInfo;
+  audioOutput?: MediaDeviceInfo;
+  videoInputs: MediaDeviceInfo[];
+  audioInputs: MediaDeviceInfo[];
+  audioOutputs: MediaDeviceInfo[];
+  play: () => void;
+};
+
+const store = SubX.proxy<StoreType>({
   ready: false,
-  devices: [] as MediaDeviceInfo[],
+  devices: [],
   async init() {
     this.devices = (await navigator.mediaDevices.enumerateDevices()).map(d =>
       d.toJSON()
+    );
+  },
+  get videoInputs(): MediaDeviceInfo[] {
+    return this.devices.filter(d => d.kind === 'videoinput');
+  },
+  get audioInputs(): MediaDeviceInfo[] {
+    return this.devices.filter(d => d.kind === 'audioinput');
+  },
+  get audioOutputs(): MediaDeviceInfo[] {
+    return this.devices.filter(d => d.kind === 'audiooutput');
+  },
+  play() {
+    console.log(
+      'play',
+      (this.videoInput ?? this.videoInputs[0]).toJSON(),
+      (this.audioInput ?? this.audioInputs[0]).toJSON(),
+      (this.audioOutput ?? this.audioInputs[0]).toJSON()
     );
   },
 });
